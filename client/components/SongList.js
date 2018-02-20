9c9f49b217a3d871f23da826fbdc1ac8c910a7c5
bauer-hillary-c <hbauer@ezcater.com>
2018-02-19 23:34:30 -0500

    bonds query to SongList component

--- client/components/SongList.js
+++ client/components/SongList.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
+import { graphql } from 'react-apollo';
 
-class SongLlist extends Component {
+class SongList extends Component {
   render() {
+    console.log(this.props);
     return (
       <div>
-        Song List
+        SongList
       </div>
     );
   }
@@ -19,4 +21,4 @@ const query = gql`
   }
 `;
 
-export default SongList;
+export default graphql(query)(SongList);
