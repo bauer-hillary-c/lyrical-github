77c6443d83c65c5d3a9dd60c4231946ce27e9f6e
bauer-hillary-c <hbauer@ezcater.com>
2018-02-19 23:06:30 -0500

    adds SongList component

---
+++ client/components/SongList.js
@@ -0,0 +1,13 @@
+import React, { Component } from 'react';
+
+class SongLlist extends Component {
+  render() {
+    return (
+      <div>
+        Song List
+      </div>
+    )
+  }
+}
+
+export default SongLlist;
