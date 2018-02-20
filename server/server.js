550cf215f52278d2019efd7b6fc337c7d7139f53
bauer-hillary-c <hbauer@ezcater.com>
2018-02-19 22:27:16 -0500

    adds MONGO_URI

--- server/server.js
+++ server/server.js
@@ -8,7 +8,7 @@ const schema = require('./schema/schema');
 const app = express();
 
 // Replace with your mongoLab URI
-const MONGO_URI = '';
+const MONGO_URI = 'mongodb://hbauer:password@ds241578.mlab.com:41578/lyricaldb';
 if (!MONGO_URI) {
   throw new Error('You must provide a MongoLab URI');
 }
