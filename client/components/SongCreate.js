28640bf9f1afee2a77b3c061d356a253b415558f
bauer-hillary-c <hbauer@ezcater.com>
2018-03-04 14:13:30 -0500

    navigate back to the index after creating a new song

--- client/components/SongCreate.js
+++ client/components/SongCreate.js
@@ -1,7 +1,7 @@
 import React, { Component } from 'react';
 import gql from 'graphql-tag';
 import { graphql } from 'react-apollo';
-import { Link } from 'react-router';
+import { Link, hashHistory } from 'react-router';
 
 class SongCreate extends Component {
   constructor(props) {
@@ -17,7 +17,8 @@ class SongCreate extends Component {
       variables: {
         title: this.state.title
       }
-    });
+    }).then(() => hashHistory.push("/"))
+      // .catch(() => "this is what you'd use to do something on failure")
   }
 
   render() {
